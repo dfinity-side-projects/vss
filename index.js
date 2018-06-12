04c7daa9c3530fe494aee82b6b612ca61da9bf1e
Perceval FARAMAZ <perfaram@windowslive.com>
2018-06-12 09:20:15 +0200

    Shares can now be renewed
    Signed-off-by: Perceval FARAMAZ <perfaram@windowslive.com>

--- index.js
+++ index.js
@@ -49,6 +49,76 @@ exports.createShare = function (bls, numOfShares, threshold) {
 }
 
 /**
+ * renew shares and verification vector, while keeping the secret
+ * @param {Object} bls - an instance of [bls-lib](https://github.com/wanderer/bls-lib)
+ * @param {Number} shares - the array of shares to be renewed (it is not possible to renew only some shares ; all must be)
+ * @param {Number} threshold - the number of share needed to recover the secret
+ * @returns {Object} the return value contains the new `verifcationVector`, an array containing the new `shares`, and the `secret` (which has not changed in respect to the original shares)
+ */
+exports.renewShare = function (bls, shares, threshold, old_vvec) {
+  // import secret
+  // generate a sk and vvec
+  const svec = []
+  const vvec = []
+
+  const zeroArray = Buffer.alloc(32)
+  const zeroSK = bls.secretKeyImport(zeroArray)
+  svec.push(zeroSK)
+
+  const zeroPK = bls.publicKey()
+  bls.getPublicKey(zeroPK, zeroSK)
+  vvec.push(zeroPK)
+
+  for (let i = 1; i < threshold; i++) {
+    const sk = bls.secretKey()
+    bls.secretKeySetByCSPRNG(sk)
+    svec.push(sk)
+
+    const pk = bls.publicKey()
+    bls.getPublicKey(pk, sk)
+    vvec.push(pk)
+  }
+
+  // generate key shares
+  const newShares = []
+  shares.forEach(share => {
+    var sk = bls.secretKey()
+    const id = bls.idImportFromInt(share.id)
+    bls.secretKeyShare(sk, svec, id)
+
+    const originalShare = bls.secretKeyImport(share.sk)
+    bls.secretKeyAdd(sk, originalShare)
+    bls.free(originalShare)
+
+    newShares.push({
+      sk: bls.secretKeyExport(sk),
+      id: share.id
+    })
+    bls.free(id)
+    bls.free(sk)
+  })
+
+  const new_vvec = old_vvec.map((pk, i) => {
+    pk = bls.publicKeyImport(pk)
+    bls.publicKeyAdd(pk, vvec[i])
+    return pk
+  })
+
+  const results = {
+    verifcationVector: new_vvec.map(pk => bls.publicKeyExport(pk)),
+    shares: newShares,
+    secret: bls.secretKeyExport(svec[0])
+  }
+
+  // clean up!
+  bls.freeArray(vvec)
+  bls.freeArray(new_vvec)
+  bls.freeArray(svec)
+
+  return results
+}
+
+/**
  * verifys a share again a verifcation vector
  * @param {Object} bls - an instance of [bls-lib](https://github.com/wanderer/bls-lib)
  * @param {Object} share - a share
--- tests/index.js
+++ tests/index.js
@@ -5,18 +5,36 @@ const bls = require('bls-lib')
 bls.onModuleInit(() => {
   tape('tests', (t) => {
     bls.init()
-    const threshold = 5
+    
+    const threshold = 4
     const numOfPlayers = 7
     const setup = vss.createShare(bls, numOfPlayers, threshold)
 
     setup.shares.forEach(share => {
       const verified = vss.verifyShare(bls, share, setup.verifcationVector)
       t.true(verified, 'should verify share')
+      console.log("Share id", share.id, ":", Buffer.from(share.sk).toString('hex'))
     })
 
-    const secret = vss.recoverSecret(bls, setup.shares.slice(0, threshold))
+    var secret = vss.recoverSecret(bls, setup.shares.slice(0, threshold))
+    console.log("Secret : ", Buffer.from(secret).toString('hex'))
 
-    t.deepEqual(secret, setup.secret, 'should recover the secert')
+    t.deepEqual(secret, setup.secret, 'should recover the secret')
+    
+    
+    const renewal = vss.renewShare(bls, setup.shares, threshold, setup.verifcationVector)
+    
+    renewal.shares.forEach(share => {
+      const verified = vss.verifyShare(bls, share, renewal.verifcationVector)
+      t.true(verified, 'should verify new share')
+      console.log("New share id", share.id, ":", Buffer.from(share.sk).toString('hex'))
+    })
+    
+    var secret = vss.recoverSecret(bls, renewal.shares.slice(0, threshold+1))
+    console.log("Secret : ", Buffer.from(secret).toString('hex'))
+    
+    t.deepEqual(setup.secret, secret, 'secret should not change after share renewal')
+    
     t.end()
   })
 })
